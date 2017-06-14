c15feba520b6726a9fddad1a5b0138150732affa
Luther <1554140485@qq.com>
2017-06-14 20:07:30 +0800

    First commit

---
+++ src/com/example/Constant.java
@@ -0,0 +1,10 @@
+package com.example;
+
+/**
+ * Created by Luther on 2017/5/24.
+ */
+public class Constant {
+
+    public static final String DB_NAME_ID = "id";
+    public static final String DB_NAME_NAME = "name";
+}
---
+++ src/com/example/MainGenerator.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.greenrobot.greendao.generator.DaoGenerator;
+import org.greenrobot.greendao.generator.Schema;
+
+/**
+ * Created by Luther on 2017/4/3.
+ */
+public class MainGenerator {
+    public static void main(String[] args) {
+        Schema schema = new Schema(1, "cn.luo.sortout.model.entity");
+        SortOutHelperDBUtil.generateSortOutHelperDB(schema);
+        schema.setDefaultJavaPackageDao("cn.luo.sortout.model.dao");
+        try {
+            String dirPath = "src";
+            new DaoGenerator().generateAll(schema, dirPath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
